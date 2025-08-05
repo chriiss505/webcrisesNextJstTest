b57f5564b8dd445106f5924dabb4ea2012856112
cristina <cristina.sanchez@vixtechnology.com>
2025-08-05 20:59:19 +0200

    Setup GitHub Pages deploy

---
+++ web-crises/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/webcrisesNextJs",
+  assetPrefix: "/webcrisesNextJs/",
+};
+
+module.exports = nextConfig;
